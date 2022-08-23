16aa94d3fea22d90f15c9baa883fec16996e8f78
guilhermeilum <guilherme220044@ilum.cnpem.br>
2022-08-23 14:18:07 -0300

    formatação do código importar_exportar

--- impotar_exportar.py
+++ impotar_exportar.py
@@ -1,23 +1,46 @@
 import pandas as pd
-def main(dados,municipio):
+
+
+def main(dados, municipio):
     dado_principal(dados)
     municipios(municipio)
 
+
 def dado_principal(dados):
-    #Primeiro iremos ler os dados do Csv, já lendo apenas as colunas já selecionadas e definindo seu tipo.
-    df = pd.read_csv(dados,sep = "\t",usecols=["infection.county","infection.state","infection.hr","notification.month","notification.year","detection.type","occupation"],dtype={"infection.hr":"category","occupation":"category","detection.type":"category","infection.state":"category"})
-    #Após apagamos todas as linhas que contem algum dado em branco, fazendo isso diminuímos de praticamente 22 milhões para 2 milhões de dados.
+    # Primeiro iremos ler os dados do Csv, já lendo apenas as colunas já selecionadas e definindo seu tipo.
+    df = pd.read_csv(
+        dados,
+        sep="\t",
+        usecols=[
+            "infection.county",
+            "infection.state",
+            "infection.hr",
+            "notification.month",
+            "notification.year",
+            "detection.type",
+            "occupation",
+        ],
+        dtype={
+            "infection.hr": "category",
+            "occupation": "category",
+            "detection.type": "category",
+            "infection.state": "category",
+        },
+    )
+    # Após apagamos todas as linhas que contem algum dado em branco, fazendo isso diminuímos de praticamente 22 milhões para 2 milhões de dados.
     df = df.dropna(axis=0)
-    #Exportamos esses dados para pickle para ficar mais fácil de fazer o upload.
+    # Exportamos esses dados para pickle para ficar mais fácil de fazer o upload.
     df.to_pickle("dados\Dados_utilizaveis")
 
+
 def municipios(municipios_dados):
-    #Para os dados dos municipios faremos os mesmos passos.
-    municipio = pd.read_csv(municipios_dados, dtype={"sigla_uf":"category"})
+    # Para os dados dos municipios faremos os mesmos passos.
+    municipio = pd.read_csv(municipios_dados, dtype={"sigla_uf": "category"})
     logic1 = municipio["ano"] >= 2009
-    logic2 = municipio["ano"] <=2019
+    logic2 = municipio["ano"] <= 2019
     municipio = municipio.loc[logic1 & logic2]
     municipio = municipio.dropna(axis=0)
     municipio.to_pickle("dados\municipio")
 
-main("dados\integrated_dataset.csv","dados\municipio.csv")
+
+main("dados\integrated_dataset.csv", "dados\municipio.csv")
